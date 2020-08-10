69c441b0d8f7b7d1cf7ce59d49078cdc5642abf9
rocioruizruiz <rocioruizruiz10@gmail.com>
2020-08-10 12:03:26 +0200

    update conftest db setting

--- app/conftest.py
+++ app/conftest.py
@@ -6,14 +6,25 @@ from django.conf import settings
 DEFAULT_ENGINE = "django.db.backends.postgresql_psycopg2"
 
 
+# @pytest.fixture(scope="session")
+# def django_db_setup():
+#     settings.DATABASES["default"] = {
+#         "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULT_ENGINE),
+#         "HOST": os.environ["DB_TEST_HOST"],
+#         "PORT": os.environ["DB_TEST_PORT"],
+#         "USER": os.environ["DB_TEST_USER"],
+#         "PASSWORD": os.environ["DB_TEST_PASSWORD"],
+#     }
+
 @pytest.fixture(scope="session")
 def django_db_setup():
     settings.DATABASES["default"] = {
-        "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULT_ENGINE),
-        "HOST": os.environ["DB_TEST_HOST"],
-        "PORT": os.environ["DB_TEST_PORT"],
-        "USER": os.environ["DB_TEST_USER"],
-        "PASSWORD": os.environ["DB_TEST_PASSWORD"],
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 
 
